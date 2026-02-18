cf21ef953da99e6f148c2ea42d0ba11731963b1a
zpkhor <zpkhor@seas.upenn.edu>
2026-02-18 09:43:16 -0500

    rf db

--- benchmark/benchmark/commands.py
+++ benchmark/benchmark/commands.py
@@ -14,7 +14,7 @@ class CommandMaker:
 
     @staticmethod
     def clean_logs():
-        return f'rm -r {PathMaker.logs_path()} ; mkdir -p {PathMaker.logs_path()}'
+        return f'rm -rf {PathMaker.logs_path()} ; mkdir -p {PathMaker.logs_path()}'
 
     @staticmethod
     def compile():
